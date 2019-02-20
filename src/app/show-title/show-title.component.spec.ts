6e6c6c7677e1016b7deade019db1916648967009
Eduardo Cavichioli <eduardocavichioli@Eduardos-MacBook-Pro.local>
2019-02-20 19:05:36 -0300

    add show-title component

---
+++ src/app/show-title/show-title.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowTitleComponent } from './show-title.component';
+
+describe('ShowTitleComponent', () => {
+  let component: ShowTitleComponent;
+  let fixture: ComponentFixture<ShowTitleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowTitleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowTitleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/show-title/show-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-title',
+  templateUrl: './show-title.component.html',
+  styleUrls: ['./show-title.component.css']
+})
+export class ShowTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
